eb4d3eeb09f482cd16c512a4793cdfc98f50107b
RISHABH GUPTA <104566113+Rishabh2503@users.noreply.github.com>
2023-11-28 12:29:58 +0530

    Adding Server

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-// console.log('server is working...');
+import express from "express";
+import morgan from "morgan";
+const app = express();
 
-// import value from "./test-name.js";
-// import someValue from "./test-default.js";
 
-// console.log(value);
-// console.log(someValue);
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post('/',(req,res) =>{
+    console.log(req);
+    res.json({message:"data received", data:req.body});
+
+});
+
+app.listen(5100, () => {
+  console.log("server run");
+});
